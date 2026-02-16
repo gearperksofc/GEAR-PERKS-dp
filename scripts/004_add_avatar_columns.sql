a6da709321ae311d22aa4d38f12ce63c7efd871a
gearperksofc <gearperksofc@gmail.com>
2026-02-16 02:09:37 -0300

    Revert "Add 4 Ultimate Guardian cards and update search filters (#22)"
    This reverts commit 721fb2b22459af78e6a63e4c479559605dd9c6c7.

--- scripts/004_add_avatar_columns.sql
+++
@@ -1,3 +0,0 @@
--- Add avatar columns to duel_rooms for live profile picture display
-ALTER TABLE duel_rooms ADD COLUMN IF NOT EXISTS host_avatar TEXT;
-ALTER TABLE duel_rooms ADD COLUMN IF NOT EXISTS guest_avatar TEXT;
